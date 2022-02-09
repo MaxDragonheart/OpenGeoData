2283f8c1d96c1f72bb3076a38c1c1a0d1164630d
max <max@drako.lan>
2022-02-09 19:10:10 +0100

    feat manege wms #21

--- website/website/gismanager/admin.py
+++ website/website/gismanager/admin.py
@@ -17,8 +17,7 @@ class WMSLayerAdmin(admin.ModelAdmin):
     search_fields = ["title"]
     prepopulated_fields = {"slug_post": ("title",)}
     fieldsets = [
-                (None, {"fields": ["title", "slug_post"]}),
-                (None, {"fields": ["description", "publishing_date"]}),
+                (None, {"fields": ["title", "slug_post", "description"]}),
                 ("WMS Parameters", {"fields": ["wms_layer_path", "wms_layer_name", "wms_layer_style"]}),
                 ("OpenLayers Parameters",
                  {"fields": ["set_max_zoom", "set_min_zoom", "set_zindex", "set_opacity"]}
