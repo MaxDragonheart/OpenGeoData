58e24a762e7d6d2e2fd872f26691e05756d07cf4
MaxDragonheart <massimilianomoraca@gmail.com>
2022-02-27 17:47:47 +0100

    feat rebuild site structure #52, created defaults data and deleted fixtures #39

--- website/website/base/utils.py
+++ website/website/base/utils.py
@@ -24,6 +24,20 @@ tags = [
     ('Protected sites', 'protected-sites', 'default/icons/006-wild-harvested-ingredients.png', 'Area designated or managed within a framework of international, Community and Member States\' legislation to achieve specific conservation objectives.'),
     ('Coordinate reference systems', 'coordinate-reference-systems', 'default/icons/007-coordinates.png', 'Systems for uniquely referencing spatial information in space as a set of coordinates (x, y, z) and/or latitude and longitude and height, based on a geodetic horizontal and vertical datum.'),
     ('Transport networks', 'transport-networks', 'default/icons/008-infrastructure.png', 'Road, rail, air and water transport networks and related infrastructure. Includes links between different networks.'),
+    ('Elevation', 'elevation', 'default/icons/009-topographic.png', 'Digital elevation models for land, ice and ocean surface. Includes terrestrial elevation, bathymetry and shoreline.'),
+    ('Geology', 'geology', 'default/icons/010-geological.png', 'Geology characterised according to composition and structure. Includes bedrock, aquifers and geomorphology.'),
+    ('Land Cover', 'land-cover', 'default/icons/011-allocate.png', 'Physical and biological cover of the earth\'s surface including artificial surfaces, agricultural areas, forests, (semi-)natural areas, wetlands, water bodies.'),
+    ('Orthoimagery', 'orthoimagery', 'default/icons/012-map.png', 'Geo-referenced image data of the Earth\'s surface, from either satellite or airborne sensors.'),
+    ('Atmospheric conditions', 'atmospheric-conditions', 'default/icons/013-meteorology.png', 'Physical conditions in the atmosphere. Includes spatial data based on measurements, on models or on a combination thereof and includes measurement locations.'),
+    ('Agricultural and acquaculture facilities', 'agricultural-and-acquaculture-facilities', 'default/icons/014-fish.png', 'Farming equipment and production facilities (including irrigation systems, greenhouses and stables).'),
+    ('Area management, restriction and regulation', 'area-management-restriction-and-regulation', 'default/icons/015-land.png', 'Areas managed, regulated or used for reporting at international, European, national, regional and local levels.'),
+    ('Bio-geographical regions', 'bio-geographical-regions', 'default/icons/016-land-1.png', 'Areas of relatively homogeneous ecological conditions with common characteristics.'),
+    ('Buildings', 'buildings', 'default/icons/017-buildings.png', 'Geographical location of buildings.'),
+    ('Environmental monitoring facilities', 'environmental-monitoring-facilities', 'default/icons/018-system.png', 'Location and operation of environmental monitoring facilities includes observation and measurement of emissions, of the state of environmental media and of other ecosystem parameters.'),
+    ('Energy resources', 'energy-resources', 'default/icons/019-renewable-energy.png', 'Energy resources including hydrocarbons, hydropower, bio-energy, solar, wind, etc., where relevant including depth/height information on the extent of the resource.'),
+    ('Habitats and biotopes', 'habitats-and-biotopes', 'default/icons/020-owl.png', 'Geographical areas characterised by specific ecological conditions, processes, structure, and (life support) functions that physically support the organisms that live there.'),
+    ('Human health and safety', 'human-health-and-safety', 'default/icons/021-hospital-sign.png', 'Geographical distribution of dominance of pathologies (allergies, cancers, respiratory diseases, etc.), information indicating the effect on health (biomarkers, decline of fertility, epidemics) or well-being of humans.'),
+    ('Land use', 'land-use', 'default/icons/022-cityscape.png', 'Territory characterised according to its current and future planned functional dimension or socio-economic purpose (e.g. residential, industrial, commercial, agricultural, forestry, recreational).'),
 ]
 
 
