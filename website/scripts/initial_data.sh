99cf88d487bcb75bd31bcb3a0e0db19bff187d7b
MaxDragonheart <massimilianomoraca@gmail.com>
2022-03-01 19:34:15 +0100

    feat docker integration #6

--- website/scripts/initial_data.sh
+++ website/scripts/initial_data.sh
@@ -4,3 +4,6 @@ echo "- Create demo data"
 python3 website/manage.py migrate
 
 cp initial_migrations/base/0002_initial_data.py website/base/migrations
+cp initial_migrations/usermanager/0002_initial_data.py website/usermanager/migrations
+
+python3 website/manage.py collectstatic
