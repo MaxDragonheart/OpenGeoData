b17d9b99f4623fadffe2b39dea12c0d6d41bead0
MaxDragonheart <massimilianomoraca@gmail.com>
2022-03-03 11:59:46 +0100

    doc #40 #42

--- website/scripts/build_tables_and_collectstatics.sh
+++ website/scripts/build_tables_and_collectstatics.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 echo "- Build DB tables"
+python3 website/manage.py makemigrations
 python3 website/manage.py migrate
 echo "- Collect static files"
 python3 website/manage.py collectstatic --noinput
