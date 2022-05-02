21076793a4b3b1d4d9d71aa7f10d8748a1efe4e0
MaxDragonheart <massimilianomoraca@gmail.com>
2022-05-02 16:32:10 +0200

    feat make fixtures #80

--- website/initial_migrations/about_migrations.md
+++
@@ -1,2 +0,0 @@
-# About Initial Migrations
-Here you can see the initial [Django migrations](https://docs.djangoproject.com/en/4.0/topics/migrations/) that contains demo data for first start of the project.
---
+++ website/scripts/restore-fixtures.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Useful only in development on local machine.
+echo "Start backup"
+
+# Usermanager
+echo "- make fixtures for Usermanager"
+python3 ./website/manage.py loaddata ../personal_website/fixtures/usermanager/userprofile.json
+
+# Standard Apps
+echo "- make fixtures for Django's standard apps"
+python3 ./website/manage.py loaddata ../website/fixtures/sites.json
+
+# Base
+echo "- make fixtures for Base"
+python3 ./website/manage.py loaddata ../website/fixtures/base/sitesocialurls.json
+python3 ./website/manage.py loaddata ../website/fixtures/base/siteurls.json
+python3 ./website/manage.py loaddata ../website/fixtures/base/sitecustomization.json
+
+echo "End process"
