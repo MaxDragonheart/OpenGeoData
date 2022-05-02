1484b3e2acaee4259a7c7b2b22525ee7b6f54459
MaxDragonheart <massimilianomoraca@gmail.com>
2022-05-02 19:40:16 +0200

    feat Create sitemap #65

--- website/website/core/sitemaps.py
+++ website/website/core/sitemaps.py
@@ -2,3 +2,63 @@ from django.contrib.sitemaps import ping_google, Sitemap
 from django.contrib.flatpages.models import FlatPage
 from django.urls import reverse
 
+from base.models import SharedCategories
+from gismanager.models import OGCLayer, WebGISProject
+
+
+class CoreSitemap(Sitemap):
+    """
+    Generic Sitemap class.
+    """
+    protocol = "https"
+
+    def save(self, force_insert=False, force_update=False):
+        super().save(force_insert, force_update)
+        try:
+            ping_google()
+        except Exception:
+            # Bare 'except' because we could get a variety
+            # of HTTP-related exceptions.
+            pass
+
+
+class CoreSitemap4Models(CoreSitemap):
+    """
+    Sitemap class for models.
+    """
+
+    def lastmod(self, obj):
+        return obj.updating_date
+
+
+######STATIC PAGE
+class StaticSitemap(CoreSitemap):
+
+    def items(self):
+        return [
+            'index',
+            'wms-list', 'map',
+        ]
+
+    def location(self, item):
+        return reverse(item)
+
+
+######BASE
+class SharedCategoriesSitemap(CoreSitemap4Models):
+
+    def items(self):
+        return SharedCategories.objects.all()
+
+
+######GISMANAGER
+class OGCLayerSitemap(CoreSitemap4Models):
+
+    def items(self):
+        return OGCLayer.objects.all()
+
+
+class WebGISProjectSitemap(CoreSitemap4Models):
+
+    def items(self):
+        return WebGISProject.objects.all()
--- website/website/core/urls.py
+++ website/website/core/urls.py
@@ -1,8 +1,21 @@
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
 
+from .sitemaps import (
+                        StaticSitemap,
+                        SharedCategoriesSitemap,
+                        OGCLayerSitemap, WebGISProjectSitemap
+                    )
+sitemaps = {
+    'staticpage': StaticSitemap,
+    'shared-category': SharedCategoriesSitemap,
+    'ogc-layer': OGCLayerSitemap,
+    'webgis-project': WebGISProjectSitemap
+}
+
+
 urlpatterns = [
-    #path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
     path('', include('base.urls')),
     path('', include('gismanager.urls')),
     path('', include('usermanager.urls')),
